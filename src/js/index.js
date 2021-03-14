f110483f8e5af527172ceb16ce9507531123c122
Khan Fayjul <khanuxd@gmail.com>
2021-03-14 16:51:45 +0200

    HTML CSS class and id's - updated based on BEM

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-import '../css/main.css';
+import "../css/main.css"
+
+let header = document.querySelector('.header__nav');
+
+// onScroll effect for Header Top
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+const scrollFunction = () => {
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
+        header.classList.add('bg');
+        buttonBackToTop.style.visibility = 'visible';
+    } else {
+        header.classList.remove('bg');
+        buttonBackToTop.style.visibility = 'hidden';
+    }
+}
